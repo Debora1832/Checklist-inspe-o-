9253edbcf18d11515a8a9919f8e49269881d73cc
DEBORA CRISTINA TAVARES RODRIGUES <debora.carmo1832@gmail.com>
2025-12-22 14:19:12 -0300

    Delete config.prod.js

--- config.prod.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfigProd = {
-    apiKey: "COLOQUE_PRODUCAO_API_KEY",
-    authDomain: "PROD.firebaseapp.com",
-    projectId: "PROD",
-    storageBucket: "PROD.appspot.com",
-    messagingSenderId: "NUMERO",
-    appId: "APP-ID",
-    measurementId: "MEASUREMENT-ID"
-};
