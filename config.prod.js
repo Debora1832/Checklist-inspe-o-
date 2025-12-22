661c25345a24481ca33174a175c7ef4c16805d1c
DEBORA CRISTINA TAVARES RODRIGUES <debora.carmo1832@gmail.com>
2025-12-22 14:39:06 -0300

    assets/js/firebase/config.prod.js

---
+++ config.prod.js
@@ -0,0 +1,9 @@
+export const firebaseConfigProd = {
+    apiKey: "COLOQUE_PRODUCAO_API_KEY",
+    authDomain: "PROD.firebaseapp.com",
+    projectId: "PROD",
+    storageBucket: "PROD.appspot.com",
+    messagingSenderId: "NUMERO",
+    appId: "APP-ID",
+    measurementId: "MEASUREMENT-ID"
+};
