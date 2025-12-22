0d37cff28939a0875f314d0af7c12580bd2ae16f
DEBORA CRISTINA TAVARES RODRIGUES <debora.carmo1832@gmail.com>
2025-12-22 14:27:30 -0300

    Delete assets directory

--- assets/js/firebase/assets/js/firebase/assets/js/firebase/config.prod.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfigProd = {
-    apiKey: "AIzaSyCExbzz_2-ttOsz8Bsixco14aSQfUKnK3w",
-    authDomain: "magius-checklist.firebaseapp.com",
-    projectId: "magius-checklist",
-    storageBucket: "magius-checklist.firebasestorage.app",
-    messagingSenderId: "377875879336",
-    appId: "1:377875879336:web:011fed4dda63f4b724f99d"
-};
