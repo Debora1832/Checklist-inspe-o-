280c911ea27031f846d389f6409a1fdc9d940e09
DEBORA CRISTINA TAVARES RODRIGUES <debora.carmo1832@gmail.com>
2025-12-22 15:23:00 -0300

    Update config.dev.js

--- assets/js/firebase/config.dev.js
+++ assets/js/firebase/config.dev.js
@@ -1,12 +1,12 @@
 /* ===========================================================
-   FIREBASE – CONFIGURAÇÃO PARA AMBIENTE DE DESENVOLVIMENTO
+   FIREBASE – CONFIG DEV (USA O MESMO DO PROD)
    =========================================================== */
 
 export const firebaseConfigDev = {
-    apiKey: "DEV-API-KEY-AQUI",
-    authDomain: "DEV-DOMAIN-AQUI.firebaseapp.com",
-    projectId: "DEV-PROJECT-ID-AQUI",
-    storageBucket: "DEV-PROJECT-ID-AQUI.appspot.com",
-    messagingSenderId: "000000000000",
-    appId: "DEV-APP-ID-AQUI"
+    apiKey: "AIzaSyCExbzz_2-ttOsz8Bsixco14aSQfUKnK3w",
+    authDomain: "magius-checklist.firebaseapp.com",
+    projectId: "magius-checklist",
+    storageBucket: "magius-checklist.firebasestorage.app",
+    messagingSenderId: "377875879336",
+    appId: "1:377875879336:web:011fed4dda63f4b724f99d"
 };
